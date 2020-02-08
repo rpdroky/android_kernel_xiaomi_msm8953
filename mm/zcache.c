63887185310021b59fb7d9239b9a850431bed570
rpdroky <rpdroky@hotmail.com>
2020-02-08 15:37:11 +0200

    Enable ZCACHE with LZ4 Compression algorythm by default - Increase max pool to 30% - Disable unneeded ZRAM
    Change-Id: I35ad5ab75994537aee8a45b74c51acdfc1197462

--- mm/zcache.c
+++ mm/zcache.c
@@ -40,22 +40,22 @@
 #include <linux/zbud.h>
 
 /*
- * Enable/disable zcache (disabled by default)
+ * Enable/disable zcache (enabled by default)
  */
-static bool zcache_enabled __read_mostly;
+static bool zcache_enabled = true;
 module_param_named(enabled, zcache_enabled, bool, 0);
 
 /*
  * Compressor to be used by zcache
  */
-#define ZCACHE_COMPRESSOR_DEFAULT "lzo"
+#define ZCACHE_COMPRESSOR_DEFAULT "lz4"
 static char *zcache_compressor = ZCACHE_COMPRESSOR_DEFAULT;
 module_param_named(compressor, zcache_compressor, charp, 0);
 
 /*
  * The maximum percentage of memory that the compressed pool can occupy.
  */
-static unsigned int zcache_max_pool_percent = 10;
+static unsigned int zcache_max_pool_percent = 30;
 module_param_named(max_pool_percent, zcache_max_pool_percent, uint, 0644);
 
 static unsigned int zcache_clear_percent = 4;
