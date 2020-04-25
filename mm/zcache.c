2107de88740117f1fec440f037f11b84f7857b36
rpdroky <rpdroky@hotmail.com>
2020-04-25 18:23:50 +0300

    Change zcache to default and re-enable zram
    Change-Id: I0964b351a25bac7830a815e16eb5905a23dc6f8e

--- mm/zcache.c
+++ mm/zcache.c
@@ -55,7 +55,7 @@ module_param_named(compressor, zcache_compressor, charp, 0);
 /*
  * The maximum percentage of memory that the compressed pool can occupy.
  */
-static unsigned int zcache_max_pool_percent = 30;
+static unsigned int zcache_max_pool_percent = 10;
 module_param_named(max_pool_percent, zcache_max_pool_percent, uint, 0644);
 
 static unsigned int zcache_clear_percent = 4;
